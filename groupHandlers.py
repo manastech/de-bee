cfddaa8cdd3495fb2008c752e821bf60fe81614e
lziliani <devnull@localhost>
2008-07-24 17:18:25 +0000

    liquidado ticket #2

--- groupHandlers.py
+++ groupHandlers.py
@@ -4,11 +4,25 @@ from model import *
 from google.appengine.api import users
 import registration as registration
 
+def authenticatedUser(req):
+	user = users.get_current_user()
+	if user is not None:
+		return True
+	else:
+		req.response.out.write("""
+      <html>
+        <body>
+		You must logging first.
+        </body>
+      </html>""")
+		return False
 
 class EnterGroupCreationHandler(webapp.RequestHandler):
 	
 	def get(self):
-		self.response.out.write("""
+    		registered = authenticatedUser(self)
+		if registered:
+			self.response.out.write("""
       <html>
         <body>
           <form action="/groupCreation" method="post">
@@ -17,31 +31,40 @@ class EnterGroupCreationHandler(webapp.RequestHandler):
           </form>
         </body>
       </html>""")
+			
 
 class GroupCreationHandler(webapp.RequestHandler):
 
 	def post(self):
 		
-		self.response.out.write('<html><body><pre>')
-		groupName = self.request.get('name')
-		escapedGroupName = cgi.escape(groupName)
+    		registered = authenticatedUser(self)
+		if registered:
+			self.response.out.write('<html><body><pre>')
+			groupName = self.request.get('name')
+			escapedGroupName = cgi.escape(groupName)
 
-		if groupName == "":
-			self.response.out.write("Group name is required")
-		else:
-			group = self.create(groupName)
-
-			if group is None:
-				self.response.out.write("The group " + escapedGroupName + " already exists. Please try another name.")
+			if groupName == "":
+				self.response.out.write("Group name is required")
 			else:
-				self.response.out.write("The group " + escapedGroupName + " has been created.")
-		self.response.out.write('</pre></body></html>')
+				group = self.createGroupAndInsertMember(groupName)
 
-	def create(self, gname):
+				if group is None:
+					self.response.out.write("The group " + escapedGroupName + " already exists. Please try another name.")
+				else:
+					self.response.out.write("The group " + escapedGroupName + " has been created.")
+			self.response.out.write('</pre></body></html>')
+
+	def insertUserInGroup(self, group):
+		membership = Membership(user=users.get_current_user(),group=group,balance=0.0)
+		membership.put()
+
+	def createGroupAndInsertMember(self, gname):
         	query = Group.gql("WHERE name = :1", gname)
         	count = query.count()
         	if count > 0:
 			return None
         	group = Group(name=gname)
         	group.put()
+
+		self.insertUserInGroup(group)
         	return group
