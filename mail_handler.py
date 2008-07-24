cbd400c27d3678bf7118bab22356e5fa3333b177
jbeyoglonian <devnull@localhost>
2008-07-24 17:40:26 +0000

    empezando con hash para rechazar transacciones

--- mail_handler.py
+++ mail_handler.py
@@ -3,6 +3,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from model import *
 from mail_sender import *
+from hashlib import *
 
 class MailHandler(webapp.RequestHandler):
 
@@ -16,3 +17,48 @@ class MailHandler(webapp.RequestHandler):
 			Ya lo mande
 			</body>
 			</html>""")
+
+
+class RejectTransactionHandler(webapp.RequestHandler):
+	def get(self):
+		key = self.request.get('key')
+		hash = self.request.get('h')
+		
+		#parameter reception validation
+		if(key is None or hash is None):
+			self.redirect('/')
+		
+		transaction = Transaction.get(key)
+		#transaction exists ?
+		if(transaction is None):
+			self.redirect('/')
+		
+		#validate hash
+		valid = TransactionHash().validate(transaction, hash)
+		
+		if valid :
+			
+			transaction.is_rejected = True
+			new_transaction = createCompensateTransaction(transaction)
+			new_transaction.put()
+			transaction.put()
+
+	def createCompensateTransaction(self,transaction):
+		pass
+		
+		
+class TransactionHash():
+	def validate(transaction, hash):
+		realHash = self.makeHash(transaction)
+		valid = hash == realHash
+		
+	def makeHash(transaction):
+		m = sha224()
+		m.update(transaction.key())
+		m.update(transaction.creator)
+		m.update(transaction.fromUser)
+		m.update(transaction.toUser)
+		m.update(transaction.type)
+		m.update(transaction.date)
+		return m.hexdigest()
+		
