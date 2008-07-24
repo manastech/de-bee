e00292930a237e09878fbedb4a9f9b08da561b5e
aborenszweig <aborenszweig@MANAS.COM.AR>
2008-07-24 15:14:28 +0000

    Espacios a tabs.

--- model.py
+++ model.py
@@ -1,21 +1,21 @@
 from google.appengine.ext import db
 
 class Group(db.Model):
-    id = db.IntegerProperty(required=True)
-    name = db.StringProperty(required=True)
+	id = db.IntegerProperty(required=True)
+	name = db.StringProperty(required=True)
     
 class Membership(db.Model):
-    user = db.UserProperty(required=True)
-    group = db.ReferenceProperty(Group)
-    balance = db.FloatProperty(required=True)
+	user = db.UserProperty(required=True)
+	group = db.ReferenceProperty(Group)
+	balance = db.FloatProperty(required=True)
     
 class Transaction(db.Model):
-    id = db.IntegerProperty()
-    creator = db.UserProperty(required=True)
-    fromUser = db.UserProperty(required=True)
-    toUser = db.UserProperty(required=True)
-    type = db.StringProperty(required=True, choices=set(["debt", "payment", "rejectedDebt", "rejectedPayment"]))
-    ammount = db.FloatProperty(required=True)
-    reason = db.StringProperty()
-    isRejected = db.BooleanProperty()
-    date = db.DateTimeProperty()
+	id = db.IntegerProperty()
+	creator = db.UserProperty(required=True)
+	fromUser = db.UserProperty(required=True)
+	toUser = db.UserProperty(required=True)
+	type = db.StringProperty(required=True, choices=set(["debt", "payment", "rejectedDebt", "rejectedPayment"]))
+	ammount = db.FloatProperty(required=True)
+	reason = db.StringProperty()
+	isRejected = db.BooleanProperty()
+	date = db.DateTimeProperty(auto_now=True)
