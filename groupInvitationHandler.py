4d3bcdf0d21bcb935f4bae0b421980d69c6fba2b
bcardiff <devnull@localhost>
2008-07-24 18:09:52 +0000

    hash for invitation

--- groupInvitationHandler.py
+++ groupInvitationHandler.py
@@ -5,8 +5,25 @@ from google.appengine.api import users
 import registration as registration
 import os
 from google.appengine.ext.webapp import template
+from hashlib import *
 
 class GroupInvitationHandler(webapp.RequestHandler):
 		
 	def get(self):
 		pass
+	
+class GroupInvitation:
+	
+	def __init__(self, group, invitedUser):
+		self.group = group
+		self.invitedUser = invitedUser
+	
+	def getUrl(self):
+		return '/groupInvitation?group=' + str(self.group.key()) + '&user=' + str(self.invitedUser.email()) + '&hash=' + self.makeHash()
+
+	def makeHash(self):
+		m = sha224()
+		m.update(str(self.group.key()))
+		m.update(self.invitedUser.email())
+		return m.hexdigest()
+	
