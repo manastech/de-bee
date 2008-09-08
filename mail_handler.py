a99c8c4dc99da2749416d9430ce13de9eb1a84ae
aromero <devnull@localhost>
2008-09-08 15:28:58 +0000

    Ticket #38

--- mail_handler.py
+++ mail_handler.py
@@ -4,50 +4,50 @@ from google.appengine.ext.webapp import template
 from hashlib import *
 from mail_sender import *
 from model import *
+from google.appengine.ext.webapp.util import login_required
 
 class RejectTransactionHandler(webapp.RequestHandler):
-
+	
+	@login_required
 	def get(self):
 		key = self.request.get('key')
 		hash = self.request.get('h')
 		
 		#parameter reception validation
-		if(key is None or hash is None):
-			self.response.out.write("Parameters")
-			return
+		if(key is not None and hash is not None):
+			transaction = Transaction.get(key)
+			#transaction exists ?
+			if(transaction is not None):
+				#validate hash
+				valid = TransactionHash().validate(transaction, hash)
+				if valid:
+					transaction.isRejected = True
+					new_transaction = self.createCompensateTransaction(transaction)
+					new_transaction.put()
+					transaction.put()
+					
+					fromMembership = Membership.gql("WHERE user = :1 AND group = :2", new_transaction.fromUser, new_transaction.group).get()
+					toMembership = Membership.gql("WHERE user = :1 AND group = :2", new_transaction.toUser, new_transaction.group).get()
+					
+					if new_transaction.type == 'rejectedPayment':
+						fromMembership.balance += new_transaction.amount
+						toMembership.balance -= new_transaction.amount
+					elif new_transaction.type == 'rejectedDebt': 
+						fromMembership.balance += new_transaction.amount
+						toMembership.balance -= new_transaction.amount
+					
+					fromMembership.put()
+					toMembership.put()
+					
+					msg = 'The transaction has been succesfully rejected!'
+					self.redirect('/group?group=%s&msg=%s' % (transaction.group.key(), msg))
+					return
 		
-		transaction = Transaction.get(key)
-		#transaction exists ?
-		if(transaction is None):
-			self.response.out.write("No Transaction")
-			return
+		# some validation has fail
+		msg = 'An error has occurred. The transaction could not be rejected.'
+		self.redirect('/?msg=%s' % msg)
 		
-		#validate hash
-		valid = TransactionHash().validate(transaction, hash)
 		
-		if valid :
-			transaction.isRejected = True
-			new_transaction = self.createCompensateTransaction(transaction)
-			new_transaction.put()
-			transaction.put()
-			
-			fromMembership = Membership.gql("WHERE user = :1 AND group = :2", new_transaction.fromUser, new_transaction.group).get()
-			toMembership = Membership.gql("WHERE user = :1 AND group = :2", new_transaction.toUser, new_transaction.group).get()
-			
-			if new_transaction.type == 'rejectedPayment':
-				fromMembership.balance += new_transaction.amount
-				toMembership.balance -= new_transaction.amount
-			elif new_transaction.type == 'rejectedDebt': 
-				fromMembership.balance += new_transaction.amount
-				toMembership.balance -= new_transaction.amount
-			
-			fromMembership.put()
-			toMembership.put()
-			
-			self.response.out.write("ok!")
-		else:
-			self.response.out.write("Is not valid")
-
 	def createCompensateTransaction(self, transaction):
 		comp_type = None
 		fromVar = transaction.fromUser
