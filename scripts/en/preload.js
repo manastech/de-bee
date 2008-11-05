e5ad158d47a7e84f5c344e8b1adb7dc1e1ea476c
aborenszweig <aborenszweig@MANAS.COM.AR>
2008-11-05 18:54:11 +0000

    Últimas imágenes en castellano. También: mejorada la precarga de imágenes.

---
+++ scripts/en/preload.js
@@ -0,0 +1,4 @@
+function preloadImage(src) {
+	var img = new Image(100, 100);
+	img.src = "/images/en/" + src;
+}
---
+++ scripts/preload.js
@@ -0,0 +1,17 @@
+if (document.images) {
+	preloadTabImages("home");
+	preloadTabImages("mygroups");
+	preloadTabImages("Actions");
+	preloadTabImages("balance");
+	preloadTabImages("History");
+	preloadTabImages("home");
+	preloadTabImages("InvitePeople");
+	preloadTabImages("mygroups");
+	preloadTabImages("properties");
+}
+
+function preloadTabImages(name) {
+	preloadImage("s_" + name + "_OFF.gif");
+	preloadImage("s_" + name + "_ON.gif");
+	preloadImage("s_" + name + "_OVER.gif");
+}
