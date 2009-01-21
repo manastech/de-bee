4650d01c6e69616c3d8bdaa62ceb57108d1a1e58
aborenszweig <aborenszweig@MANAS.COM.AR>
2009-01-21 06:54:25 +0000

    Paginado en who

--- who.py
+++ who.py
@@ -6,7 +6,24 @@ from model import Group
 class WhoHandler(webapp.RequestHandler):
 	
 	def get(self):
-		for group in Group.all():
+		page = self.request.get('p');
+		if page is None or page == '':
+			page = 1
+		else:
+			page = int(page)
+			
+		offset = (page - 1) * 10
+		
+		if page != 1:
+			self.response.out.write("<a href=\"?p=%s\">Previous</a> | " % (page - 1))
+			
+		self.response.out.write(" &nbsp; %s &nbsp; " % page)
+			
+		self.response.out.write(" | <a href=\"?p=%s\">Next</a>" % (page + 1))
+		
+		self.response.out.write("<br/><br/>")
+		
+		for group in Group.gql("LIMIT %s, %s" % (offset, 10)):
 			self.response.out.write("<b>%s</b>" % group.name)
 			
 			self.response.out.write("<ul>")
